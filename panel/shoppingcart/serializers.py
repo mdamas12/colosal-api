76bee54caf63808e9f3fc4fca11b00dd83aa0f49
Marcos Damas <marcosdamas12@gmail.com>
2021-02-04 15:36:29 -0400

    modulo carrito de compra

---
+++ panel/shoppingcart/serializers.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+
+from panel.products.serializers import ProductDetailSerializer
+
+from .models import *
+
+class ShoppingcartSerializer(serializers.ModelSerializer):
+    #customer = serializers.IntegerField(read_only=True)
+    class Meta:
+        model = Shoppingcart
+        fields = ('__all__')
+
+class ShoppingcartDetailSerializer(serializers.ModelSerializer):
+    #customer = serializers.IntegerField(read_only=True)
+    product = ProductDetailSerializer(many=False)
+    class Meta:
+        model = Shoppingcart
+        fields = (
+            'id',
+            'customer',
+            'product',
+            'quantity',
+            'amount',
+            'status',
+
+        )
+
+
+class ShcCustomerSerializer(serializers.ModelSerializer):
+    shoppingcart = ShoppingcartDetailSerializer(many=True)
+
+    class Meta:
+        model = Customer
+        fields = (
+            'id',
+            'fullname',
+            'shoppingcart'
+        )
