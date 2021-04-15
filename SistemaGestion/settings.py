c56a5ccc2b2fbea50043602866836e301e983599
Luis Tundisi <luistundisi@gmail.com>
2021-04-14 22:35:19 -0400

    subiendo settings

--- SistemaGestion/settings.py
+++ SistemaGestion/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'lkam^9(ld!z-!35!+)oi@xyr!9_(d%m^8(_axfd5d93x3ex(ll'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["161.35.123.177","localhost"]
+ALLOWED_HOSTS = ["161.35.123.177","localhost","minimarketcolosal.com"]
 
 
 # Application definition
